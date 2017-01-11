ba17f8cbde1bc778e570c14be7345d33512c7c10
alefebvre-fixit <antoinelefebvre@gmail.com>
2017-01-10 23:53:43 -0800

    more and charts

--- src/pages/story/story-progress.ts
+++ src/pages/story/story-progress.ts
@@ -16,13 +16,22 @@ export class StoryProgressPage {
 
   public story: Story;
   public day: number = 0;
+
+  public initialProgress = 0;
   public dailyProgress = 0;
+  public remaining = 0;
 
     // Doughnut
-  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [350, 450, 100];
+  public doughnutChartLabels:string[] = ['progress', 'daily', 'remaining'];
+  public doughnutChartData:number[] = [0, 0, 1];
   public doughnutChartType:string = 'doughnut';
 
+
+  //https://github.com/valor-software/ng2-charts/issues/251
+  //http://stackoverflow.com/questions/20966817/how-to-add-text-inside-the-doughnut-chart-using-chart-js
+  public colors = [
+                "#FF6384",
+            ];
 
   constructor(
     public storyService: StoryService,
@@ -37,6 +46,17 @@ export class StoryProgressPage {
     let storyId = this.params.get("storyId");
     this.storyService.getStory(storyId).subscribe((story: Story) => {
       this.story = story;
+
+      this.initialProgress = story.progress;
+      if (this.initialProgress == undefined){
+        this.initialProgress = 0;
+        story.progress = 0;
+      }
+      
+      this.dailyProgress = 0;
+      this.story.size - this.story.progress;
+      this.updateChart(this.initialProgress, this.dailyProgress, this.remaining);
+
     });
   }
 
@@ -53,12 +73,38 @@ export class StoryProgressPage {
   }
 
   addProgress(){
-    this.dailyProgress++;
+    this.progress(+1);
+  }
+ 
+  removeProgress(){
+    this.progress(-1);
   }
 
-  removeProgress(){
-    this.dailyProgress--;
-  }
+  progress(increment: number){
+    this.dailyProgress =  this.dailyProgress + increment ;
+
+    let daily = this.dailyProgress++
+    let remaining = this.story.size - this.story.progress;
+
+    if (daily > remaining){
+      daily = remaining;
+    }
+
+    this.dailyProgress = daily;
+    this.remaining = this.story.size - this.story.progress - daily;
+    
+    this.updateChart(this.initialProgress, this.dailyProgress, this.remaining);
+ 
+ }
+
+ updateChart(progress: number, daily: number, remaining: number){
+    this.doughnutChartData= [progress, daily, remaining];
+ }
+
+
+
+
+
 
   apply(){
 
